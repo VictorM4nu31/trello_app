f95d65a153a2d90a1500913e1c92780e55cf3fc5
VictorM4nu31 <angelesvictor690@gmail.com>
2024-09-25 18:45:29 -0600

    0.01 cambios en el login se impleneta el almacenamiento

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:logger/logger.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final Logger _logger = Logger(); // Instancia de Logger
+  final Logger _logger = Logger();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   // Registrar usuario con correo y contraseña
   Future<User?> registerWithEmailAndPassword(String email, String password) async {
@@ -18,6 +23,27 @@ class AuthService {
     } catch (e) {
       _logger.e('Error al registrar usuario: $e');
       return null;
+    }
+  }
+
+  // Guardar perfil del usuario en Firestore
+  Future<void> saveUserProfile(String uid, String name, String surname, File? profileImage) async {
+    try {
+      String? photoUrl;
+      if (profileImage != null) {
+        final storageRef = _storage.ref().child('profileImages/$uid.jpg');
+        await storageRef.putFile(profileImage);
+        photoUrl = await storageRef.getDownloadURL();
+      }
+
+      await _firestore.collection('users').doc(uid).set({
+        'name': name,
+        'surname': surname,
+        'photoUrl': photoUrl,
+      });
+      _logger.i('Perfil guardado con éxito');
+    } catch (e) {
+      _logger.e('Error al guardar el perfil del usuario: $e');
     }
   }
 
--- lib/ui/screens/register_screen.dart
+++ lib/ui/screens/register_screen.dart
@@ -1,17 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import '../../services/auth_service.dart'; // Importa tu AuthService
+import 'task_screen.dart'; // Importa TaskScreen
 
-class RegisterScreen extends StatelessWidget {
-  // Añadir el constructor con el parámetro 'key'
-  const RegisterScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key}); // Convert 'key' to a super parameter
+
+  @override
+  RegisterScreenState createState() => RegisterScreenState();
+}
+
+class RegisterScreenState extends State<RegisterScreen> {
+  final _authService = AuthService(); // Instancia del servicio de autenticación
+  final _nameController = TextEditingController();
+  final _surnameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  File? _profileImage;
+  final ImagePicker _picker = ImagePicker();
+
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _register() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      if (_passwordController.text != _confirmPasswordController.text) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Las contraseñas no coinciden')),
+        );
+        return;
+      }
+
+      final user = await _authService.registerWithEmailAndPassword(
+        _emailController.text,
+        _passwordController.text,
+      );
+
+      if (user != null) {
+        await _authService.saveUserProfile(
+          user.uid,
+          _nameController.text,
+          _surnameController.text,
+          _profileImage,
+        );
+        if (mounted) { // Verifica si el widget está montado
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Usuario registrado con éxito')),
+          );
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => TaskScreen()),
+          );
+        }
+      }
+    }
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor ingrese un correo electrónico';
+    }
+    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+    if (!emailRegex.hasMatch(value)) {
+      return 'Por favor ingrese un correo electrónico válido';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor ingrese una contraseña';
+    }
+    if (value.length < 8) {
+      return 'La contraseña debe tener al menos 8 caracteres';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Register'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: const Center(
-        child: Text('Register Screen'),
+      body: SingleChildScrollView( // Añadir SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: _pickImage,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage:
+                        _profileImage != null ? FileImage(_profileImage!) : null,
+                    child: _profileImage == null
+                        ? const Icon(Icons.add, size: 50)
+                        : null,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                const Text('Agregar foto de perfil'),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Nombre'),
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _surnameController,
+                  decoration: const InputDecoration(labelText: 'Apellidos'),
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: 'Correo Electrónico'),
+                  validator: _validateEmail,
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(labelText: 'Contraseña'),
+                  obscureText: true,
+                  validator: _validatePassword,
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  decoration: const InputDecoration(labelText: 'Confirmar Contraseña'),
+                  obscureText: true,
+                  validator: _validatePassword,
+                ),
+                const SizedBox(height: 32.0),
+                ElevatedButton(
+                  onPressed: _register,
+                  child: const Text('Registrar'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
