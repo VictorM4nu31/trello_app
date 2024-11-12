7afa91da67991d185b29f60ee6b07c07fbc60f0c
layronBot <gutierrezlugolayron046@gmail.com>
2024-11-11 18:57:53 -0600

    0.10 corrección en actualizar foto de perfil y contraseña

--- lib/ui/screens/edit_profile_screen.dart
+++ lib/ui/screens/edit_profile_screen.dart
@@ -70,26 +70,30 @@ class EditProfileScreenState extends State<EditProfileScreen> {
           final ref = _storage.ref().child('profile_images/${user.uid}');
           await ref.putFile(_profileImage!);
           _photoUrl = await ref.getDownloadURL();
+          
+          await _firestore.collection('users').doc(user.uid).update({
+            'photoUrl': _photoUrl,
+          });
+        }
+
+        if (_passwordController.text.isNotEmpty) {
+          await user.updatePassword(_passwordController.text);
         }
 
         await user.verifyBeforeUpdateEmail(_emailController.text);
         await _firestore.collection('users').doc(user.uid).update({
           'name': _nameController.text,
           'surname': _surnameController.text,
-          'photoUrl': _photoUrl,
         });
-
-        setState(() {});
 
         if (mounted) {
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text('Perfil actualizado con éxito')),
           );
-          Navigator.of(context).pop(true); // Regresar a la pantalla anterior y pasar un valor true
+          Navigator.of(context).pop(); // Regresa a la pantalla anterior
         }
       } catch (e) {
         if (mounted) {
-          // Verifica si el widget está montad
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text('Error al actualizar el perfil')),
           );
